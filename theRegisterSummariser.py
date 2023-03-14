8bac0fada29855cdd569a91c2269b687fe8f0e3b
Blah <michaelaguilera12345@gmail.com>
2023-03-14 10:06:27 +0100

    Update theRegisterSummariser.py

--- theRegisterSummariser.py
+++ theRegisterSummariser.py
@@ -10,7 +10,7 @@ openai.api_key = "<CHANGE_ME>"
 def summarize_text(text):
     response = openai.Completion.create(
         engine="text-davinci-003",
-        prompt=f"Summarize the following text:\n\n{text}\n\nSummary:",
+        prompt=f"Please summarize the following text:\n\n{text}\n\nSummary:",
         max_tokens=500,
         n=1,
         stop=None,
@@ -31,17 +31,30 @@ page = requests.get(url, headers=headers)
 soup = BeautifulSoup(page.content, "html.parser")
 articles = soup.find_all("article")
 
-# Prompt the user to select an article
-print("Please select an article to summarize:")
-print("0. Summarize all articles")
-for i, article in enumerate(articles):
-    title = article.find("h4").get_text()
-    print(f"{i+1}. {title}")
-selection = int(input("\n>>> ")) - 1
+# Extract all article tags
+tags = []
+for article in articles:
+    tag = article.find("span", {"class": "section_name"}).get_text()
+    if tag not in tags:
+        tags.append(tag)
 
-if selection == -1:
-    # Summarize all articles
-    for i, article in enumerate(articles):
+# Prompt the user to select an option
+print("Please select an option:")
+print("1. Summarize articles under specified tag")
+print("2. Select an article to summarize")
+selection = int(input("\n>>> "))
+
+if selection == 1:
+    # Prompt the user to select a tag
+    print("Please select a tag to view articles:")
+    for i, tag in enumerate(tags):
+        print(f"{i+1}. {tag}")
+    selection = int(input("\n>>> ")) - 1
+    selected_tag = tags[selection]
+
+    # Extract all articles with the selected tag and summarize their text
+    selected_articles = [article for article in articles if article.find("span", {"class": "section_name"}).get_text() == selected_tag]
+    for i, article in enumerate(selected_articles):
         link = "https://www.theregister.com" + article.find("a")["href"]
         article_page = requests.get(link, headers=headers)
         article_soup = BeautifulSoup(article_page.content, "html.parser")
@@ -50,14 +63,35 @@ if selection == -1:
         summary = summarize_text(article_text)
         print(f"\n\nTitle ({i+1}):", title)
         print(summary)
+
 else:
-    # Extract the selected article's information and summarize the text
-    selected_article = articles[selection]
-    link = "https://www.theregister.com" + selected_article.find("a")["href"]
-    article_page = requests.get(link, headers=headers)
-    article_soup = BeautifulSoup(article_page.content, "html.parser")
-    article_text = "".join([p.get_text() for p in article_soup.find_all("p")])
-    title = article_soup.title.string
-    summary = summarize_text(article_text)
-    print("\n\nTitle:", title)
-    print(summary)
+    # Prompt the user to select an article or summarize all articles
+    print("Please select an article to summarize:")
+    print("0. Summarize all articles")
+    for i, article in enumerate(articles):
+        title = article.find("h4").get_text()
+        print(f"{i+1}. {title}")
+    selection = int(input("\n>>> ")) - 1
+
+    if selection == -1:
+        # Summarize all articles
+        for i, article in enumerate(articles):
+            link = "https://www.theregister.com" + article.find("a")["href"]
+            article_page = requests.get(link, headers=headers)
+            article_soup = BeautifulSoup(article_page.content, "html.parser")
+            article_text = "".join([p.get_text() for p in article_soup.find_all("p")])
+            title = article_soup.title.string
+            summary = summarize_text(article_text)
+            print(f"\n\nTitle ({i+1}):", title)
+            print(summary + '\n\n')
+    else:
+        # Extract the selected article's information and summarize the text
+        selected_article = articles[selection]
+        link = "https://www.theregister.com" + selected_article.find("a")["href"]
+        article_page = requests.get(link, headers=headers)
+        article_soup = BeautifulSoup(article_page.content, "html.parser")
+        article_text = "".join([p.get_text() for p in article_soup.find_all("p")])
+        title = article_soup.title.string
+        summary = summarize_text(article_text)
+        print("\n\nTitle:", title)
+        print(summary + '\n\n')
